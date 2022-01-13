3d35524bc878dcc07d5addab2aff724ba9753037
Alina Muliak <70890265+alinamuliak@users.noreply.github.com>
2022-01-13 16:06:03 +0200

    updated queue.h

--- firmware/MedButton/queue.h
+++ firmware/MedButton/queue.h
@@ -8,6 +8,9 @@
 #define MESSAGE_SIZE 40
 
 
-void add_to_queue(char info_queue[QUEUE_SIZE][MESSAGE_SIZE], const char *element);
-void print_queue(char info_queue[QUEUE_SIZE][MESSAGE_SIZE]);
+void add_time(char info_queue[QUEUE_SIZE][MESSAGE_SIZE], const char *element);
+void add_latitute(float info_queue[QUEUE_SIZE], float element);
+void add_longtitude(float info_queue[QUEUE_SIZE], float element);
+void add_pulse(int info_queue[QUEUE_SIZE], int element);
+//void print_queue(int info_queue[QUEUE_SIZE]);
 #endif
